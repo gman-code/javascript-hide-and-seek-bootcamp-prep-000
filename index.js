9a381e01ce47083d3411a93a83e97801b16c67f3
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-25 22:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+function getFirstSelector(selector) {
+  var element = document.querySelector(selector);
+  return element
+}
+
+ function nestedTarget() {
+ var nestedTarget = document.querySelector('#nested .target')
+ return nestedTarget
+}
+
+function deepestChild() {
+  var deepestChild = document.querySelector('#grand-node div div div div')
+  return deepestChild
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+      const firstList = rankedLists[0]
+      const secondList = rankedLists[1]
+      let children = firstList.children
+
+for (let i = 0; i < children.length; i++) {
+      children[i].innerHTML = parseInt(children[i].innerHTML) + n
+      }
+
+children = secondList.children
+
+      for (let i = 0; i < children.length; i++) {
+        children[i].innerHTML = parseInt(children[i].innerHTML) + n
+      }
+
+}
